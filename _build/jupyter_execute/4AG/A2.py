88b99c03babfa275277f71830b5624ac2dba187e
StraightDraw <robbsinn@gmail.com>
2021-03-10 17:48:48 -0500

    added 4ag sec 4

--- _build/jupyter_execute/4AG/A2.py
+++ _build/jupyter_execute/4AG/A2.py
@@ -1,9 +1,9 @@
-# A.2 Proofs using Analytic Geometry
+# A.2 Proofs in Analytic Geometry
 
-For the following theorems, use analytic geometric techniques to prove the following theorems.
+Use analytic geometric techniques to prove the following well-known Euclidean geometry theorems. Make sure to pick the origin and axes so that the arbitrary figure is located helpfully with respect to the axes and has a useful orientation. Then use Euclidean theorems and equations of lines to prove the needed result.
 
 ## Theorems
-1. A quadrilateral ABCD is a parallelogram if and only if one pair of opposite sides are congruent and parallel.
+1. A quadrilateral ABCD is a parallelogram if and only if one pair of opposite sides are congruent.
     ````{hint}
     Most analytic geometry proofs are made easier if we remember that we choose the coordinate system to make our life easier. For the above proof, place the line segment $\overline{AB}$ on the $x$-axis such that $B=(0,0)$ and $A=(a,0)$. Locate $C$ and $D$ in the first quadrant.
     
@@ -16,35 +16,10 @@ For the following theorems, use analytic geometric techniques to prove the follo
     ```
     
     ````
-    We intentionally pick the origin and axes so that an arbitrary figure is located helpfully with respect the axes and has a useful orientation. Then use Euclidean theorems and equations of lines to prove the needed result.
-    
-2. Midpoint Formula. The midpoint $M$ of line segment $\overline{AB}$ where $A(x_1,y_1)$ and $B(x_2,y_2)$ is given by:$$M(x,y)=\left( \frac{x_1+x_2}{2},\frac{y_1+y_2}{2} \right)$$
-3. The diagonals of a parallelogram bisect each other. 
+2. The diagonals of a parallelogram bisect each other. 
 
     ````{hint}
     Use same coordinate setup shown above.
     ````
 
-4. The set of all rectangles with area equal to $1$ which have a vertex at the origin satisfy the relationship $xy=1$.
-
-## Conic Sections
-We can derive basic formulas for the conic sections using simplifications, then generate more robust formulas later once we have worked the details of transformations.
-
-1. The equation of a circle centered at the origin is given by $x^2+y^2=r^2$.
-
-
-    Parabola
-    : A parabola is a set of points $P\in\mathbb P$ which are equidistant from a focal point $F$ and a directrix line $L$.
-
-
-2. Suppose we have a parabola with a vertical line of symmtery through the origin (e.g. $x=0$) and focal point $F=(0,y_0)$ where $y_0>0$. Prove that the set of all points $P=(x,y)$ on the parabola is equivalent to 
-
-    $$x^2+(y-y_0)^2=(y+y_0)^2$$
-
-    and that the directrix line for the parabola has the equation 
-
-    $$y=\frac{1}{y_0}$$
-
-3. Hyperbola...page under construction...coming soon.
-
-
+3. The set of all rectangles with area equal to $1$ which have a vertex at the origin satisfy the relationship $y=\frac{1}{x}$.
---
+++ _build/jupyter_execute/4AG/A4.py
@@ -0,0 +1,38 @@
+# A.4 Conic Sections
+
+We can derive basic formulas for the conic sections using simplifications, then generate more robust formulas later once we have worked the details of transformations.
+
+Circle
+: The set of all points equidistant from a point called the *center*.
+
+Radius
+: The common distance from a point on a circle to its center.
+
+Parabola
+: A set of points which are equidistant from a *focal point* $F$ and a line $l$ called the *directrix*.
+
+Hyperbola
+: A set of points where the absolute difference of the distances from two points (called the *foci*) is constant.
+
+
+## Theorems
+
+1. The equation of a circle centered at the origin with radius $r$ is given by 
+
+    $$x^2+y^2=r^2$$
+
+2. The equation of a circle centered at the point $(h,k)$ with radius $r$ is given by 
+
+    $$(x-h)^2+(y-k)^2=r^2$$
+
+3. Create a segment $FP$ that is perpendicular to the directrix and passes through the focal point $F$. The midpoint of $FP$ is on the parabola.
+
+4. Suppose we have a parabola with a vertical line of symmtery through the origin (e.g. $x=0$) and focal point $F=(0,y_0)$ where $y_0>0$. Prove that the set of all points $P=(x,y)$ on the parabola is equivalent to 
+
+    $$x^2+(y-y_0)^2=(y+y_0)^2$$
+
+    and that the directrix line for the parabola has the equation 
+
+    $$y=\frac{1}{y_0}$$
+
+3. Hyperbola...under construction...coming soon.
